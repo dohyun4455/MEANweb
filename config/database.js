6f0201644113022afbbe575ad0e90410caf9ee56
dohyun4455 <44558385+dohyun4455@users.noreply.github.com>
2018-11-05 11:49:12 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-//  database: 'mongodb://localhost:27017/meanauth_dohyun',
-  database : 'mongodb://dohyun4455:do4455@ds131973.mlab.com:31973/meanauthapp2',
-  secret: 'do4455'
+  database : '모자이크',
+  secret: '모자이크'
 }
